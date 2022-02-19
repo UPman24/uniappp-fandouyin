7c58d1b3345f406d922ac06480c709167dbae4ce
UPman24 <66861298+UPman24@users.noreply.github.com>
2022-02-19 15:00:17 +0800

    Add files via upload

---
+++ fandouyin/App.vue
@@ -0,0 +1,18 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+			plus.screen.lockOrientation("portrait-primary");
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
